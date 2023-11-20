66a454e70634d4fbfe84cc2ea658ca685f53ef55
Kwabena A  Frimpong <kafrimpong23@gmail.com>
2023-11-20 14:35:58 +0000

    Parent Class First Implementation Completed

--- models/base_model.py
+++ models/base_model.py
@@ -1,5 +1,40 @@
 #!venv/bin/python3
 """Base Model Class For WalkiThot"""
+import datetime
+import uuid
+
 
 class BaseModel:
-    """BaseModel Class"""
+    """BaseModel Class:
+
+    Attributes:
+        id (string): Unique identifier
+        created_at (datetime): date created
+        update_at (datetime): date updated
+    """
+
+    def __init__(self):
+        """Initializes the BaseModel class"""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
+
+    def __str__(self):
+        """Returns [<class name>] (<self.id>) <self.__dict__>"""
+        return f"[{self.__class__.__name__}] ({self.id}) {str(self.__dict__)}"
+
+    def save(self):
+        """Updates public instance attribute update_at with current datetime"""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """Returns a dictionary of all keys/values of
+        __dict__ of the instance"""
+        instance_dict = {}
+        for key, value in self.__dict__.items():
+            if key == 'created_at' or key == 'updated_at':
+                instance_dict[key] = value.isoformat()
+            else:
+                instance_dict[key] = value
+        instance_dict['__class__'] = self.__class__.__name__
+        return instance_dict
