6c56578af17b9452336cca04da31ee6672f1d930
Kwabena A  Frimpong <kafrimpong23@gmail.com>
2023-11-21 10:26:42 +0000

    category enum added

--- models/blog.py
+++ models/blog.py
@@ -1,6 +1,24 @@
 #!venv/bin/python3
 """Blog Class Module"""
 from models.base_model import BaseModel
+from enum import Enum
+
+
+class Category(Enum):
+    """
+    Enumeration representing categories for blog posts.
+
+    Each value in the enum corresponds to a category.
+    """
+    DEVELOPMENT = "Web & Mobile Development"
+    CLOUD = "Cloud Computing"
+    AI = "Artificial Intelligence & Machine Learning"
+    DATA = "Data Science & Analytics"
+    CYBERSECURITY = "Cybersecurity"
+    IOT = "Internet of Things(Iot)"
+    NEWS = "Tech News & Trends"
+    ENTREPRENEURSHIP = "Entrepreneurship"
+    OTHER = "Other..."
 
 
 class Blog(BaseModel):
@@ -8,9 +26,9 @@ class Blog(BaseModel):
 
     Attributes:
         title (string): Blog post Title
-        type (pending): Category blog post falls under
+        type (Category): Category blog post falls under
         content (string): Blog post
     """
     title = ""
-    type = ""
+    type = None
     content = ""
