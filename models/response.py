2b78815ad44034505d1451aac82905b2075dde95
Kwabena A  Frimpong <kafrimpong23@gmail.com>
2023-11-25 11:50:07 +0000

    Response class fully implemented

--- models/response.py
+++ models/response.py
@@ -1,18 +1,26 @@
 #!venv/bin/python3
 """Response Class Module"""
-from models.base_model import BaseModel
+from sqlalchemy import ForeignKey, String
+from sqlalchemy.orm import relationship, mapped_column
+from models.base_model import Base, BaseModel
+import models
 
 
-class Response(BaseModel):
+class Response(BaseModel, Base):
     """Response Class:
 
     Attributes:
         comment_id (string): Foreign Key (comment.id)
         reply (string): Response to a comment under a blog post
     """
-
-    comment_id = ""
-    reply = ""
+    if models.storage_t == 'db':
+        __tablename__ = 'responses'
+        comment_id = mapped_column(String(60), ForeignKey('comments.id'))
+        reply = mapped_column(String(255))
+        comment = relationship("Comment", back_populates="responses")
+    else:
+        comment_id = ""
+        reply = ""
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
